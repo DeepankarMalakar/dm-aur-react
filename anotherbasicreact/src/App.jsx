ec2727bdfe1153cfb2326c840f90f84d6c4d5d41
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-08-03 20:11:07 +0530

    react learning

---
+++ anotherbasicreact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Learning React - DM | Setup using vite</h1>
+  )
+}
+
+export default App
---
+++ basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Learning React - DM | Setup using create-react-app name</h1>
+  );
+}
+
+export default App;
